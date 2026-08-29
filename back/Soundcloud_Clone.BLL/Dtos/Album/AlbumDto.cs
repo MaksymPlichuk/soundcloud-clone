2d1826f7d0f66c72b4df60fc4d8ee2e7d1a89856
Maksym <pilipm449@gmail.com>
2026-08-29 16:36:04 +0300

    add dtos and mapperly

---
+++ back/Soundcloud_Clone.BLL/Dtos/Album/AlbumDto.cs
@@ -0,0 +1,19 @@
+﻿using Soundcloud_Clone.BLL.Dtos.Auth;
+using Soundcloud_Clone.BLL.Dtos.Song;
+using Soundcloud_Clone.DAL.Enitites;
+using Soundcloud_Clone.DAL.Enitites.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soundcloud_Clone.BLL.Dtos.Album
+{
+    public class AlbumDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public UserForInfoDto Author { get; set; }
+        public List<SongForInfo> Songs { get; set; } = [];
+    }
+}
---
+++ back/Soundcloud_Clone.BLL/Dtos/Auth/UserForInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soundcloud_Clone.BLL.Dtos.Auth
+{
+    public class UserForInfoDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string? Image { get; set; }
+
+    }
+}
