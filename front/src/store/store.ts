7f590bb239d797e2d5ee8c395821c89631865fbe
Maksym <pilipm449@gmail.com>
2026-09-02 10:17:05 +0300

    feat: Progress bar Play btn
    fixed progress btn added progress bar added Album pages mock

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -7,12 +7,18 @@ type PlayerStore = {
     volume: number;
     isLiked: boolean;
 
+    currentTime: number,
+    duration: number,
+
     play: (song: ISongItem | null) => void;
     pause: () => void;
     togglePlay: () => void;
     //next: () => void; в майбутньому
     //previous: () => void;
     setVolume: (volume: number) => void;
+
+    setCurrentTime: (currentTime: number) => void;
+    setDuration: (duration: number) => void;
 }
 
 export const usePlayerStore = create<PlayerStore>((set) => ({
@@ -21,10 +27,16 @@ export const usePlayerStore = create<PlayerStore>((set) => ({
     volume: 100,
     isLiked: false,
 
+    currentTime: 0,
+    duration: 0,
+
     setVolume: (volume: number) => {
         set(() => ({volume}));
     },
     play: (song) => set(() => ({currentTrack: song, isPlaying: true})),
-    pause: () => set(()=>({isPlaying:false})),
-    togglePlay: () => set(()=>({isPlaying:true})),
+    pause: () => set(() => ({isPlaying: false})),
+    togglePlay: () => set((state) => ({isPlaying: !state.isPlaying})),
+
+    setCurrentTime: (time) => set(() => ({currentTime: time})),
+    setDuration: (duration) => set(() => ({duration})),
 }));
