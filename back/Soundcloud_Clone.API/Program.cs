aa9beca91fe9316e486173999f688398734dd59d
P C <denismirzaev12@gmail.com>
2026-09-03 23:47:49 +0300

    feat: connect Neon Postgres DB and apply migrations on startup

--- back/Soundcloud_Clone.API/Program.cs
+++ back/Soundcloud_Clone.API/Program.cs
@@ -81,5 +81,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 await app.SeedAsync();
 app.Run();
